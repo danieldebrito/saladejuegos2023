233f4f0fcb4116e25016218476b512100dfe87e9
daniel de brito <danieldebrito@outlook.com>
2023-05-04 22:31:14 -0300

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: '/home',
+    pathMatch: 'full',
+  },
+  { path: 'home', loadChildren: () => import('./pages/home/home.module').then(m => m.HomeModule) },
+  { path: 'about', loadChildren: () => import('./pages/about/about.module').then(m => m.AboutModule) },
+  { path: 'auth/login', loadChildren: () => import('./pages/auth/login/login.module').then(m => m.LoginModule) },
+  { path: '**', loadChildren: () => import('./pages/error404/error404.module').then(m => m.Error404Module) },
+  { path: 'error', loadChildren: () => import('./pages/error404/error404.module').then(m => m.Error404Module) },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ src/environments/environment.ts
@@ -0,0 +1,23 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  firebase: {
+    projectId: 'saladejuegos-9c63e',
+    appId: '1:1079705165150:web:dfd7dc849fbde0454883cf',
+    storageBucket: 'saladejuegos-9c63e.appspot.com',
+    apiKey: 'AIzaSyDMpUmTRCc0uaOOmRE8QOZC9nBTAsfkUk4',
+    authDomain: 'saladejuegos-9c63e.firebaseapp.com',
+    messagingSenderId: '1079705165150',
+  },
+
+};
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/plugins/zone-error';  // Included with Angular CLI.
