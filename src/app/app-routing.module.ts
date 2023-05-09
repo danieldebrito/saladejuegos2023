311371a385c54fede69eb880c30eca63ba3412af
daniel de brito <danieldebrito@outlook.com>
2023-05-08 23:43:52 -0300

    folders

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -13,6 +13,10 @@ const routes: Routes = [
   { path: 'sign-up', loadChildren: () => import('./auth/sign-up/sign-up.module').then(m => m.SignUpModule) },
   { path: 'forgot-password', loadChildren: () => import('./auth/forgot-password/forgot-password.module').then(m => m.ForgotPasswordModule) },
   { path: 'verify-email', loadChildren: () => import('./auth/verify-email/verify-email.module').then(m => m.VerifyEmailModule) },
+  { path: 'chat', loadChildren: () => import('./pages/chat/chat.module').then(m => m.ChatModule) },
+  { path: 'games/ahorcado', loadChildren: () => import('./pages/games/ahorcado/ahorcado.module').then(m => m.AhorcadoModule) },
+  { path: 'games/preguntados', loadChildren: () => import('./pages/games/preguntados/preguntados.module').then(m => m.PreguntadosModule) },
+  { path: 'games/mayormenor', loadChildren: () => import('./pages/games/mayormenor/mayormenor.module').then(m => m.MayormenorModule) },
   { path: '**', loadChildren: () => import('./pages/error404/error404.module').then(m => m.Error404Module) },
   { path: 'error', loadChildren: () => import('./pages/error404/error404.module').then(m => m.Error404Module) },
 ];
