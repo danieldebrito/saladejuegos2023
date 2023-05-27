19c355a1175420a0c4b4f958c902750a0558418d
daniel de brito <danieldebrito@outlook.com>
2023-05-27 17:48:19 -0300

    ahorcado

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,25 +3,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { environment } from '../environments/environment';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HttpClientModule } from '@angular/common/http';
+
 // firebase /////////////////////////////////////////////////////////////
 import { initializeApp,provideFirebaseApp } from '@angular/fire/app';
 import { provideAuth,getAuth } from '@angular/fire/auth';
 import { provideDatabase,getDatabase } from '@angular/fire/database';
 import { provideFirestore,getFirestore } from '@angular/fire/firestore';
 import { FIREBASE_OPTIONS } from '@angular/fire/compat';
-
-
-/*
-import { AngularFireModule } from '@angular/fire/compat';
-import { AngularFireAuthModule } from '@angular/fire/compat/auth';
-import { AngularFireStorageModule } from '@angular/fire/compat/storage';
-import { AngularFirestoreModule } from '@angular/fire/compat/firestore';
-import { AngularFireDatabaseModule } from '@angular/fire/compat/database';
-*/
-/////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////
 import { LayoutModule } from './layout/layout.module';
 import { AuthService } from './services/auth.service';
-
 
 @NgModule({
   declarations: [
@@ -31,17 +23,11 @@ import { AuthService } from './services/auth.service';
     BrowserModule,
     AppRoutingModule,
     LayoutModule,
+    HttpClientModule,
     provideFirebaseApp(() => initializeApp(environment.firebase)),
     provideAuth(() => getAuth()),
     provideDatabase(() => getDatabase()),
     provideFirestore(() => getFirestore()),
-/*
-    AngularFireModule.initializeApp(environment.firebase),
-    AngularFireAuthModule,
-    AngularFirestoreModule,
-    AngularFireStorageModule,
-    AngularFireDatabaseModule,
-    */
   ],
   providers: [
     AuthService,
--- src/app/pages/games/ahorcado/ahorcado.component.ts
+++ src/app/pages/games/ahorcado/ahorcado.component.ts
@@ -1,10 +1,86 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { PalabrasService } from 'src/app/services/palabras.service';
 
 @Component({
   selector: 'app-ahorcado',
   templateUrl: './ahorcado.component.html',
   styleUrls: ['./ahorcado.component.scss']
 })
-export class AhorcadoComponent {
+export class AhorcadoComponent implements OnInit {
 
+  public palabra: string = '';
+  public palabraArray: string[] = [];
+
+  public palabraTipeada: string = '------';
+  public palabraTipeadaArray: string[] = ['-', '-', '-', '-', '-', '-'];
+
+  public letraTipeada: string = '';
+
+  public mostrarPalabra = false;
+  public cantIntentos = 5;
+
+  public mensaje = 'Te Quedan : ';
+
+  constructor(private palabrasSv: PalabrasService) { }
+
+  public getPalabra() {
+    this.palabrasSv.get().subscribe(res => {
+      this.palabra = res;
+      this.palabraArray = this.palabra.toString().split('');
+    });
+  }
+
+  public LetraTipeada(letra: string) {
+    this.letraTipeada = letra;
+    this.checkLetra();
+  }
+
+  public checkLetra() {
+
+    let flag = true;
+
+    for (let index = 0; index < this.palabraArray.length; index++) {
+      if (this.palabraArray[index].toLowerCase() == this.letraTipeada.toLowerCase()) {
+        this.palabraTipeadaArray[index] = this.letraTipeada;
+        flag = false;
+      }
+    }
+
+    if (flag) {
+      this.cantIntentos -= 1;
+    }
+
+    this.palabraTipeada = this.arrayToPalabra(this.palabraTipeadaArray);
+    this.MostrarPalabra(5);
+  }
+
+  public arrayToPalabra(palabra: string[]) {
+    let ret = '';
+    for (let index = 0; index < palabra.length; index++) {
+      ret += palabra[index];
+    }
+    return ret;
+  }
+
+  public MostrarPalabra(intentos: number) {
+    if (this.cantIntentos == intentos) {
+      this.mostrarPalabra = true;
+    }
+  }
+
+  public ResetJuego() {
+    this.cantIntentos = 5;
+    this.mostrarPalabra = false;
+    this.palabraTipeada = '------';
+    this.getPalabra();
+    this.palabra = '';
+    this.palabraArray = [];
+
+    this.palabraTipeadaArray = ['-', '-', '-', '-', '-', '-'];
+    this.letraTipeada = '';
+  }
+
+  ngOnInit(): void {
+    this.getPalabra();
+  }
 }
